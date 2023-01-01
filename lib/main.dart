54c01769a324c9a76c4fb057afbe706bf6c251ea
JacDM <jackdanielmoseshs@gmail.com>
2023-01-01 20:37:44 +0400

    Final commit for unity Android

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:geotagar/screens/unityAR.dart';
+import 'package:geotagar/screens/default.dart';
 
 void main() {
   runApp(const MyApp());
@@ -13,7 +14,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: UnityAR(),
+      home: const DefaultMyApp(),
     );
   }
 }
