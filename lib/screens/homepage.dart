300dd70a010a27dcb30098a49f96f922b2cbac6e
ms2019 <ms2019@hw.ac.uk>
2023-01-04 23:23:42 +0400

    Temporary Homepage
    Created a temporary homepage with bottom navigation bar.

--- lib/screens/homepage.dart
+++ lib/screens/homepage.dart
@@ -7,9 +7,66 @@ class HomePage extends StatefulWidget {
   State<HomePage> createState() => _HomePageState();
 }
 
+// Temporary homepage, will be modified later
 class _HomePageState extends State<HomePage> {
+  int _currentIndex = 0;
+
+  List<Widget> body = const [
+    Icon(Icons.home),
+    Icon(Icons.location_on),
+    Icon(Icons.circle_outlined, size: 50),
+    Icon(Icons.groups),
+    Icon(Icons.person),
+  ];
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Container(
+        // Temporary background image
+        decoration: const BoxDecoration(
+            image: DecorationImage(
+                image: AssetImage('assets/images/LogIn_bg.png'),
+                fit: BoxFit.cover)),
+        child: Scaffold(
+            resizeToAvoidBottomInset: false,
+            backgroundColor: Colors.transparent,
+            body: Center(
+              child: body[_currentIndex],
+            ),
+            bottomNavigationBar: SizedBox(
+                height: 83,
+                child: BottomNavigationBar(
+                  currentIndex: _currentIndex,
+                  onTap: (index) {
+                    setState(() {
+                      _currentIndex = index;
+                    });
+                  },
+                  items: const [
+                    BottomNavigationBarItem(
+                      icon: Icon(Icons.home),
+                      label: 'Home',
+                      backgroundColor: Colors.deepOrange,
+                    ),
+                    BottomNavigationBarItem(
+                      icon: Icon(Icons.location_on),
+                      label: 'Location',
+                      backgroundColor: Colors.blueAccent,
+                    ),
+                    BottomNavigationBarItem(
+                        icon: Icon(Icons.circle_outlined, size: 50),
+                        label: '',
+                        backgroundColor: Colors.greenAccent),
+                    BottomNavigationBarItem(
+                      icon: Icon(Icons.groups),
+                      label: 'Discover',
+                      backgroundColor: Colors.purpleAccent,
+                    ),
+                    BottomNavigationBarItem(
+                      icon: Icon(Icons.person),
+                      label: 'Profile',
+                      //backgroundColor: Colors.red,
+                    ),
+                  ],
+                ))));
   }
 }
