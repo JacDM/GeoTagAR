6160003052a523f7f4a4d1aef564378a103ef009
ms2019 <ms2019@hw.ac.uk>
2023-01-11 12:18:01 +0400

    Updated main() with async
    Included firebase parts in main

--- lib/main.dart
+++ lib/main.dart
@@ -1,13 +1,20 @@
 import 'package:flutter/material.dart';
-import 'package:geotagar/screens/homepage.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:geotagar/firebase_options.dart';
 import 'package:geotagar/screens/log_in.dart';
+
+import 'package:geotagar/screens/homepage.dart';
 import 'package:geotagar/screens/unityAR.dart';
 import 'package:geotagar/screens/default.dart';
 import 'package:geotagar/screens/userAccountScreens/privacy_and_security.dart';
 import 'package:geotagar/screens/userAccountScreens/user_profile.dart';
 import 'package:geotagar/screens/userAccountScreens/user_settings.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   runApp(const MyApp());
 }
 
@@ -20,7 +27,7 @@ class MyApp extends StatelessWidget {
     return const MaterialApp(
       debugShowCheckedModeBanner: false,
       //home: UnityAR(),
-      home: UnityAR(),
+      home: LogIn(),
     );
   }
 }
