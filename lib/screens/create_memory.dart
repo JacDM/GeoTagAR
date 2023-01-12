52ccdb7e55afaadf3153fa502ef289e7c49f23d4
praveen <pv2003@hw.ac.uk>
2023-01-12 21:31:56 +0400

    new file for creating memory

---
+++ lib/screens/create_memory.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class CreateMemory extends StatefulWidget {
+  const CreateMemory({super.key});
+
+  @override
+  State<CreateMemory> createState() => _CreateMemoryState();
+}
+
+class _CreateMemoryState extends State<CreateMemory> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.black,
+    );
+  }
+}
