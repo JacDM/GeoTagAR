6e2fa3f3c8d6c690199370ca39bf60835b41487c
GauriR06 <90549257+GauriR06@users.noreply.github.com>
2023-01-17 20:42:36 +0400

    Added selection property

--- lib/screens/userAccountScreens/acc_visibility.dart
+++ lib/screens/userAccountScreens/acc_visibility.dart
@@ -3,6 +3,12 @@ import 'package:geotagar/screens/userAccountScreens/reusableWidgets/reusable_con
 
 Widget k_divider = Divider(color: Colors.grey[900], height: 0.5, thickness: 0.5, endIndent: 20.0, indent: 20.0,);
 const k_textStyle = TextStyle(fontSize: 20.0);
+const k_selectedtextStyle = TextStyle(fontSize: 20.0, fontWeight: FontWeight.bold);
+const k_containerColor = Color(0xFFBDBDBD);  //grey[400]
+
+enum Options{
+  option1, option2, option3, option4
+}
 
 class AccVisibility extends StatefulWidget {
   const AccVisibility({super.key});
@@ -12,11 +18,21 @@ class AccVisibility extends StatefulWidget {
 }
 
 class _AccVisibility extends State<AccVisibility> {
+  TextStyle bg_option1 = k_textStyle;
+  TextStyle bg_option2 = k_textStyle;
+  TextStyle bg_option3 = k_textStyle;
+  TextStyle bg_option4 = k_textStyle;
 
+
+  void deselectOptions (Options op) {
+    bg_option1 = op == Options.option1 ? k_selectedtextStyle : k_textStyle;
+    bg_option2 = op == Options.option2 ? k_selectedtextStyle : k_textStyle;
+    bg_option3 = op == Options.option3 ? k_selectedtextStyle : k_textStyle;
+    bg_option4 = op == Options.option4 ? k_selectedtextStyle : k_textStyle;
+  }
 
   @override
   Widget build(BuildContext context) {
-
 
     return Scaffold(
       //appbar
@@ -44,42 +60,71 @@ class _AccVisibility extends State<AccVisibility> {
                 SizedBox(
                   height: MediaQuery.of(context).size.height * .30,
                   child: ReusableContainer(
-                    colour: Colors.grey,
+                    colour: k_containerColor,  //grey[400]
                     containerChild: Column(
                       //mainAxisAlignment: MainAxisAlignment.center,
                       crossAxisAlignment: CrossAxisAlignment.start,
                       children: <Widget> [
-                        const Expanded(
-                          child: ReusableContainer(
-                            colour: Colors.grey,
-                            containerChild: Text('Everyone', style: k_textStyle,),
+
+                        Expanded(
+                          child: GestureDetector(
+                            onTap: () {
+                              setState(() {
+                                deselectOptions(Options.option1);
+                              });
+                            },
+                            child: ReusableContainer(
+                              colour: k_containerColor,
+                              containerChild: Text('Everyone', style: bg_option1),
+                            ),
                           ),
                         ),
 
                         k_divider,
 
-                        const Expanded(
-                          child: ReusableContainer(
-                            colour: Colors.grey,
-                            containerChild: Text('Only My Contacts', style: k_textStyle,),
+                        Expanded(
+                          child: GestureDetector(
+                            onTap: () {
+                              setState(() {
+                                deselectOptions(Options.option2);
+                              });
+                            },
+                            child: ReusableContainer(
+                              colour: k_containerColor,
+                              containerChild: Text('Only My Contacts', style: bg_option2,),
+                            ),
                           ),
                         ),
 
                         k_divider,
 
-                        const Expanded(
-                          child: ReusableContainer(
-                            colour: Colors.grey,
-                            containerChild: Text('Only My Contacts except...', style: k_textStyle,),
+                        Expanded(
+                          child: GestureDetector(
+                            onTap: () {
+                              setState(() {
+                                deselectOptions(Options.option3);
+                              });
+                            },
+                            child: ReusableContainer(
+                              colour: k_containerColor,
+                              containerChild: Text('Only My Contacts except...', style: bg_option3,),
+                            ),
                           ),
                         ),
 
                         k_divider,
 
-                        const Expanded(
-                          child: ReusableContainer(
-                            colour: Colors.grey,
-                            containerChild: Text('Nobody', style: k_textStyle,),
+                        Expanded(
+                          child: GestureDetector(
+                            onTap: () {
+                              setState(() {
+                                deselectOptions(Options.option4);
+                              });
+                            },
+                            child: ReusableContainer(
+                              colour: k_containerColor,
+                              containerChild: Text('Nobody', style: bg_option4,),
+                            ),
                           ),
                         ),
 
