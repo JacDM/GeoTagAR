5b49ac4dc4eedc8f51727688c3448d0c07b2a9b0
praveen <pv2003@hw.ac.uk>
2023-01-19 12:28:59 +0400

    implemented half of the post page

--- lib/screens/memory_related/post_memory.dart
+++ lib/screens/memory_related/post_memory.dart
@@ -1,15 +1,59 @@
+import 'package:camera/camera.dart';
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
+import 'package:geotagar/main.dart';
 
 class AddPost extends StatefulWidget {
-  const AddPost({super.key});
+  const AddPost({
+    super.key,
+    required this.camCtrl,
+    //required this.image
+  });
+
+  final CameraController camCtrl;
+  //final XFile image;
 
   @override
   State<AddPost> createState() => _AddPostState();
 }
 
 class _AddPostState extends State<AddPost> {
+  Uint8List? _file;
+
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.white70,
+        leading: IconButton(
+          onPressed: () {
+            Navigator.of(context).pop();
+          },
+          icon: Icon(
+            Icons.arrow_back_ios,
+            color: Colors.black,
+          ),
+        ),
+        title: const Text('Post Memory'),
+        centerTitle: false,
+      ),
+      backgroundColor: Colors.white54,
+      body: Container(
+        child: Column(children: [
+          //linear indicator
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceAround,
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [CircleAvatar()],
+          )
+        ]),
+      ),
+    );
+    // Center(
+    //   child: IconButton(
+    //     icon: const Icon(Icons.upload),
+    //     onPressed: () {},
+    //   ),
+    // );
   }
 }
