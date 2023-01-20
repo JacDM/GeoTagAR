8cd0a80278e16a5097ca31472360a845856a775b
ms2019 <ms2019@hw.ac.uk>
2023-01-21 00:09:51 +0400

    Temporary discover pages
    Temporary pages to test upload functionality to web firebase storage (not android).

---
+++ lib/screens/discoverPages/group_page.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/src/widgets/container.dart';
+import 'package:flutter/src/widgets/framework.dart';
+
+class GroupPage extends StatefulWidget {
+  const GroupPage({super.key});
+
+  @override
+  State<GroupPage> createState() => _GroupPageState();
+}
+
+class _GroupPageState extends State<GroupPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
