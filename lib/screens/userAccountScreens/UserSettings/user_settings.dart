c481430bf006c4f2996966acdd91eeeb18643963
GauriR06 <90549257+GauriR06@users.noreply.github.com>
2023-01-22 16:27:13 +0400

    Minor changes in appbar title

--- lib/screens/userAccountScreens/UserSettings/user_settings.dart
+++ lib/screens/userAccountScreens/UserSettings/user_settings.dart
@@ -55,7 +55,7 @@ class _SettingsPageState extends State<SettingsPage> {
     return Scaffold(
       //appbar
       appBar: AppBar(
-        title: const Center(child: Text('SETTINGS')),
+        title: const Text('SETTINGS'),
       ),
 
 
