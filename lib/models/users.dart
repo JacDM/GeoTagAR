77a9bec8286ec95a7409d42d7d38327175ca98a4
GauriR06 <90549257+GauriR06@users.noreply.github.com>
2023-01-24 20:14:02 +0400

    Created separate file for user info

---
+++ lib/models/users.dart
@@ -0,0 +1,57 @@
+class User {
+    final String email;
+    final String uid;
+    final String password;
+    final Strign photoUrl;
+    final String username;
+    final String bio;
+    final String gender;
+    final String age;
+    // final String followers;
+    // final String following;
+    // final String posts;
+
+    const User({
+        required this.email,
+        required this.uid,
+        required this.password,
+        required this.photoUrl,
+        required this.username,
+        required this.bio,
+        required this.gender,
+        required this.age,
+        // required this.followers,
+        // required this.following,
+        // required this.posts,
+    });
+
+    Map<String, dynamic> toJson() => {
+        "username" : username,
+        "password" : password,
+        "uid" : uid,
+        "email" : email,
+        "photoUrl" : photoUrl,
+        "bio" : bio,
+        "gender" : gender,
+        "age" : age,
+        // "followers" : followers,
+        // "following" : following,
+        // "posts" : posts,
+    };
+}
+
+//Add this in Register.dart -> add user to database
+// import this file as models;
+
+// models.User user = models.User(
+//     username : username,
+//     password : password,
+//     uid : cred.user!.uid,
+//     email : email,
+//     photoUrl : photoUrl,
+//     bio : bio,
+//     gender : gender,
+//     age : age,
+// );
+
+// await FirebaseFirestore.instance.collection('users').doc(cred.user!.uid).set(user.toJson(),);
