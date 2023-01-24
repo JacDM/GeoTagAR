3167e38d9bc11410b797e289db0d797a412763f3
ms2019 <ms2019@hw.ac.uk>
2023-01-24 22:44:43 +0400

    Added cloud firestore package
    Added cloud firestore package and fixed 'strign' typo.

--- lib/models/users.dart
+++ lib/models/users.dart
@@ -1,10 +1,11 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 
 class User {
     final String email;
     final String uid;
     final String password;
-    final Strign photoUrl;
+    final String photoUrl;
     final String username;
     final String bio;
     final String gender;
