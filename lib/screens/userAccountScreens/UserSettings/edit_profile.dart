222cb3d0208a621bdbae4c0b2821dddf6ec56ce5
GauriR06 <90549257+GauriR06@users.noreply.github.com>
2023-01-26 13:28:12 +0400

    Minor changes in layout

--- lib/screens/userAccountScreens/UserSettings/edit_profile.dart
+++ lib/screens/userAccountScreens/UserSettings/edit_profile.dart
@@ -1,7 +1,15 @@
 import 'package:flutter/material.dart';
+import '../reusableWidgets/custom_button.dart';
 
 const Color k_subHeadingsColor = Color(0xFF004D40);   //teal[900]
+TextStyle k_textstyle = const TextStyle(fontSize: 24.0, fontWeight: FontWeight.bold,);
 const Color k_fontTextColour = Colors.black;
+const k_username = '@DATTEBAYOOO';
+const k_name = 'Uzumaki Naruto';
+const k_bio = 'Naruto Uzumaki, a young ninja who seeks recognition from his peers and dreams of becoming the Hokage, the leader of his village.';
+
+Widget k_verSpacing =  const SizedBox(height: 30.0,);
+
 
 class EditProfile extends StatefulWidget {
   const EditProfile({super.key});
@@ -26,183 +34,143 @@ class _EditProfileState extends State<EditProfile> {
 
       body: SingleChildScrollView(
         child: SafeArea(
-          child: Padding(
-            padding: const EdgeInsets.all(20.0),
-            child: Column(
-              //mainAxisAlignment: MainAxisAlignment.center,
-              children: <Widget>[
+          child: Column(
+            //mainAxisAlignment: MainAxisAlignment.center,
+            children:  <Widget>[
 
-                const SizedBox(
-                  height: 10.0,
-                ),
+              //Edit profile and bg pic
+              Stack(
+                clipBehavior: Clip.none,
+                //alignment: Alignment.center,
+                children:  [
 
-                //Profile Picture
-                const CircleAvatar(
-                  radius: 100.0,
-                  backgroundImage: NetworkImage(
-                  'https://static.wikia.nocookie.net/naruto/images/d/dc/Naruto%27s_Sage_Mode.png/revision/latest/scale-to-width-down/1920?cb=20150124180545'),
-                ),
-
-                //Edit Profile Picture
-                TextButton(
-                  onPressed: null,
-
-                  child: Row(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    children: const <Widget> [
-                      Text('Change Profile Picture  ',
-                        style: TextStyle(
-                          //fontWeight: FontWeight.bold,
-                          fontSize: 20.0,
-                          //fontFamily: 'FiraCode',
-                          color: k_subHeadingsColor,
-                        ),
-                      ),
-
-                      // CircleAvatar(
-                      //   radius: 20.0,
-                      //   backgroundColor: Colors.white70,
-                      //   child: Icon(
-                      //     Icons.edit ,
-                      //     size: 25.0,
-                      //     color: k_subHeadingsColor,
-                      //   ),
-                      // ),
-
-                      Icon(
-                          Icons.edit ,
-                          size: 25.0,
-                          color: k_subHeadingsColor,
-                        ),
-                    ],
+                  //Background Picture
+                  const Image(
+                    image: NetworkImage(
+                        'https://images.squarespace-cdn.com/content/v1/5fe4caeadae61a2f19719512/1612119994906-GFOPIE3ZKXB79DS6A612/Naruto43.jpg'),
                   ),
-                ),
-                const SizedBox(
-                  height: 30.0,
-                ),
 
-                //Username
-                const Text('@DATTEBAYOOO',
-                  style: TextStyle(
-                    //fontWeight: FontWeight.bold,
-                    fontSize: 25.0,
-                    //fontFamily: 'FiraCode',
-                    color: k_fontTextColour,
-                  ),
-                ),
-                TextButton(
-                  onPressed: null,
-
-                  child: Row(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    children: const [
-                      Text('Edit Username ',
-                        style: TextStyle(
-                          //fontWeight: FontWeight.bold,
-                          fontSize: 20.0,
-                          //fontFamily: 'FiraCode',
-                          color: k_subHeadingsColor,
-                        ),
-                      ),
-
-
-                      Icon(
+                  //Edit
+                  Positioned(
+                    top: 10.0,
+                    right: 10.0,
+                    child: CircleAvatar(
+                      radius: MediaQuery.of(context).size.width * 0.0525,
+                      backgroundColor: Colors.white,
+                      child: Icon(
                         Icons.edit ,
-                        size: 25.0,
-                        color: k_subHeadingsColor,
-                        ),
-
-                    ],
-                  ),
-                ),
-                const SizedBox(
-                  height: 30.0,
-                ),
-
-                //Name
-                const Text('Uzumaki Naruto',
-                  style: TextStyle(
-                    //fontWeight: FontWeight.bold,
-                    fontSize: 25.0,
-                    //fontFamily: 'FiraCode',
-                    color: k_fontTextColour,
-                  ),
-                ),
-                TextButton(
-                  onPressed: null,
-
-                  child: Row(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    children: const [
-                      Text('Edit Name ',
-                        style: TextStyle(
-                          //fontWeight: FontWeight.bold,
-                          fontSize: 20.0,
-                          //fontFamily: 'FiraCode',
-                          color: k_subHeadingsColor,
-                        ),
-                      ),
-
-
-                      Icon(
-                        Icons.edit ,
-                        size: 25.0,
+                        size: MediaQuery.of(context).size.width * 0.08,
                         color: k_subHeadingsColor,
                       ),
+                    ),
 
-                    ],
                   ),
-                ),
-                const SizedBox(
-                  height:20.0,
-                ),
 
-                //Bio
-                const Padding(
-                       padding: EdgeInsets.all(20.0),
-                       child: ListTile(
-                         title: Text(
-                           'Naruto Uzumaki, a young ninja who seeks recognition from his peers and dreams of becoming the Hokage, the leader of his village.',
-                           style: TextStyle(
-                             //fontFamily: 'FiraCode',
-                             fontSize: 20.0,
-                             //fontWeight: FontWeight.bold,
-                           ),
-                         ),
-                       ),
-                     ),
-                TextButton(
-                  onPressed: null,
+                  //Profile Picture
+                  Positioned(
+                    bottom: -55.0,
+                    right: MediaQuery.of(context).size.width * 0.35,
+                    child: Stack(
+                      clipBehavior: Clip.none,
+                      children:  [
+                        CircleAvatar(
+                          radius: MediaQuery.of(context).size.width * 0.14,
+                          backgroundImage: NetworkImage('https://static.wikia.nocookie.net/naruto/images/d/dc/Naruto%27s_Sage_Mode.png/revision/latest/scale-to-width-down/1920?cb=20150124180545'),
+                        ),
 
-                  child: Row(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    children: const [
-                      Text('Edit Bio/Description ',
-                        style: TextStyle(
-                          //fontWeight: FontWeight.bold,
-                          fontSize: 20.0,
-                          //fontFamily: 'FiraCode',
-                          color: k_subHeadingsColor,
+                        Positioned(
+                          bottom: -9.0,
+                          left: MediaQuery.of(context).size.width * 0.20,
+
+                          child: CircleAvatar(
+                            radius: MediaQuery.of(context).size.width * 0.0425,
+                            backgroundColor: Colors.white,
+                            child: Icon(
+                              Icons.edit ,
+                              size: MediaQuery.of(context).size.width * 0.07,
+                              color: k_subHeadingsColor,
+                            ),
+                          ),
+                        ),
+                      ],
+
+                    ),
+                  ),
+                ],
+              ),
+
+              k_verSpacing,
+              k_verSpacing,
+
+
+
+              Padding(
+                padding: const EdgeInsets.all(20.0),
+                child: Column(
+                  children: [
+                    //Username
+                    Text('USERNAME', style: k_textstyle),
+                    TextField(
+                      textAlign: TextAlign.left,
+                      controller: TextEditingController(text: k_username),
+                      style: const TextStyle(fontSize: 20.0, height: 0.75),
+                      onSubmitted: null,
+                    ),
+                    k_verSpacing,
+
+
+                    //Name
+                    Text('NAME', style: k_textstyle),
+                    TextField(
+                      textAlign: TextAlign.left,
+                      controller: TextEditingController(text: k_name),
+                      style: const TextStyle(fontSize: 20.0, height: 0.75),
+                      onSubmitted: null,
+                    ),
+                    k_verSpacing,
+
+                    //Bio
+                    //Text Box
+                    Text('BIO', style: k_textstyle),
+                    const SizedBox(
+                      height: 20.0,
+                    ),
+                    TextField(
+                      style: const TextStyle(fontSize: 20.0,),
+                      maxLines: 6,
+                      textAlign: TextAlign.left,
+                      maxLength: 500,
+                      controller: TextEditingController(text: k_bio),
+                      decoration: const InputDecoration(
+                        border: OutlineInputBorder(),
+                        //labelText: 'Type here...',
+                      ),
+                    ),
+
+                    const SizedBox(
+                      height: 20.0,
+                    ),
+
+
+                    RaisedGradientButton(
+                      gradient: const LinearGradient(
+                        colors: <Color>[Colors.deepPurple, Colors.purple],
+                      ),
+                      onPressed: () => Navigator.pop(context),
+                      child: const Center(
+                        child: Text(
+                          'SUBMIT',
+                          style: TextStyle(color: Colors.white, fontSize: 20.0, fontWeight: FontWeight.w500),
                         ),
                       ),
-
-
-                      Icon(
-                        Icons.edit ,
-                        size: 25.0,
-                        color: k_subHeadingsColor,
-                      ),
-
-                    ],
-                  ),
+                    ),
+                  ],
                 ),
-                const SizedBox(
-                  height: 30.0,
-                ),
+              ),
 
-              ],
-            ),
-          ),
+
+                  ],
+                ),
         ),
       ),
 
