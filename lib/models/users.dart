9b660b96eb16af6dbd04a5cad161d42ee3be85e3
ms2019 <ms2019@hw.ac.uk>
2023-01-27 04:45:50 +0400

    Updated user model with a few things

--- lib/models/users.dart
+++ lib/models/users.dart
@@ -1,62 +1,70 @@
+//import 'dart:ffi';
+
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 
 class User {
-    final String email;
-    final String uid;
-    final String password;
-    final Strign photoUrl;
-    final String username;
-    final String bio;
-    final String gender;
-    final String age;
-    // final String followers;
-    // final String following;
-    // final String posts;
+  final String email;
+  final String uid;
+  final String name;
+  //final String password;
+  //final String photoUrl;
+  final String username;
+  //final String bio;
+  final String gender;
+  final String accountType;
+  //final int age;
+  // final String followers;
+  // final String following;
+  // final String posts;
 
-    const User({
-        required this.email,
-        required this.uid,
-        required this.password,
-        required this.photoUrl,
-        required this.username,
-        required this.bio,
-        required this.gender,
-        required this.age,
-        // required this.followers,
-        // required this.following,
-        // required this.posts,
-    });
+  const User({
+    required this.email,
+    required this.uid,
+    required this.name,
+    //required this.password,
+    //required this.photoUrl,
+    required this.username,
+    //required this.bio,
+    required this.gender,
+    required this.accountType,
+    //required this.age,
+    // required this.followers,
+    // required this.following,
+    // required this.posts,
+  });
 
-    Map<String, dynamic> toJson() => {
-        "username" : username,
-        "password" : password,
-        "uid" : uid,
-        "email" : email,
-        "photoUrl" : photoUrl,
-        "bio" : bio,
-        "gender" : gender,
-        "age" : age,
+  Map<String, dynamic> toJson() => {
+        "username": username,
+        //"password": password,
+        "name": name,
+        "uid": uid,
+        "email": email,
+        //"photoUrl": photoUrl,
+        //"bio": bio,
+        "gender": gender,
+        "accountType": accountType,
+        //"age": age,
         // "followers" : followers,
         // "following" : following,
         // "posts" : posts,
-    };
+      };
 
-
-    static User fromSnap(DocumentSnapshot snap) {
-        var snapshot = snap.data() as Map <String, dynamic>;
-        return User(
-            username : snapshot['username'],
-            password : snapshot['password'],
-            uid : snapshot['uid'],
-            email : snapshot['email'],
-            photoUrl : snapshot['photoUrl'],
-            bio : snapshot['bio'],
-            gender : snapshot['gender'],
-            age : snapshot['age'],
-        );
-    }
-
-
+  static User fromSnap(DocumentSnapshot snap) {
+    var snapshot = snap.data() as Map<String, dynamic>;
+    return User(
+      username: snapshot['username'],
+      //password: snapshot['password'],
+      name: snapshot['name'],
+      uid: snapshot['uid'],
+      email: snapshot['email'],
+      //photoUrl: snapshot['photoUrl'],
+      //bio: snapshot['bio'],
+      gender: snapshot['gender'],
+      //age: snapshot['age'],
+      accountType: snapshot['accountType'],
+    );
+  }
 }
 
 //Add this in Register.dart -> add user to database
