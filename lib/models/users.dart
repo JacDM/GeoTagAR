795c5ee393e91c82f4fd5ebaf1c7c2f50f6008eb
ms2019 <ms2019@hw.ac.uk>
2023-01-30 14:33:03 +0400

    Updated user model to accommodate more values

--- lib/models/users.dart
+++ lib/models/users.dart
@@ -1,84 +1,119 @@
+// ignore_for_file: public_member_api_docs, sort_constructors_first
 //import 'dart:ffi';
 
 import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 
-class User {
+class UserModel {
   final String email;
   final String uid;
   final String name;
-  //final String password;
-  //final String photoUrl;
+  final String profilePicture;
+  final String banner;
+
   final String username;
-  //final String bio;
+
   final String gender;
   final String accountType;
-  //final int age;
-  // final String followers;
-  // final String following;
-  // final String posts;
-
-  const User({
+  final List<String> awards;
+  const UserModel({
     required this.email,
     required this.uid,
     required this.name,
-    //required this.password,
-    //required this.photoUrl,
+    required this.profilePicture,
+    required this.banner,
     required this.username,
-    //required this.bio,
     required this.gender,
     required this.accountType,
-    //required this.age,
-    // required this.followers,
-    // required this.following,
-    // required this.posts,
+    required this.awards,
   });
 
-  Map<String, dynamic> toJson() => {
-        "username": username,
-        //"password": password,
-        "name": name,
-        "uid": uid,
-        "email": email,
-        //"photoUrl": photoUrl,
-        //"bio": bio,
-        "gender": gender,
-        "accountType": accountType,
-        //"age": age,
-        // "followers" : followers,
-        // "following" : following,
-        // "posts" : posts,
-      };
 
-  static User fromSnap(DocumentSnapshot snap) {
-    var snapshot = snap.data() as Map<String, dynamic>;
-    return User(
-      username: snapshot['username'],
-      //password: snapshot['password'],
-      name: snapshot['name'],
-      uid: snapshot['uid'],
-      email: snapshot['email'],
-      //photoUrl: snapshot['photoUrl'],
-      //bio: snapshot['bio'],
-      gender: snapshot['gender'],
-      //age: snapshot['age'],
-      accountType: snapshot['accountType'],
+  UserModel copyWith({
+    String? email,
+    String? uid,
+    String? name,
+    String? profilePicture,
+    String? banner,
+    String? username,
+    String? gender,
+    String? accountType,
+    List<String>? awards,
+  }) {
+    return UserModel(
+      email: email ?? this.email,
+      uid: uid ?? this.uid,
+      name: name ?? this.name,
+      profilePicture: profilePicture ?? this.profilePicture,
+      banner: banner ?? this.banner,
+      username: username ?? this.username,
+      gender: gender ?? this.gender,
+      accountType: accountType ?? this.accountType,
+      awards: awards ?? this.awards,
     );
+  }
+
+  Map<String, dynamic> toMap() {
+    return <String, dynamic>{
+      'email': email,
+      'uid': uid,
+      'name': name,
+      'profilePicture': profilePicture,
+      'banner': banner,
+      'username': username,
+      'gender': gender,
+      'accountType': accountType,
+      'awards': awards,
+    };
+  }
+
+  factory UserModel.fromMap(Map<String, dynamic> map) {
+    return UserModel(
+      email: (map["email"] ?? '') as String,
+      uid: (map["uid"] ?? '') as String,
+      name: (map["name"] ?? '') as String,
+      profilePicture: (map["profilePicture"] ?? '') as String,
+      banner: (map["banner"] ?? '') as String,
+      username: (map["username"] ?? '') as String,
+      gender: (map["gender"] ?? '') as String,
+      accountType: (map["accountType"] ?? '') as String,
+      awards: List<String>.from(((map['awards'] ?? const <String>[]) as List<String>),),
+    );
+  }
+
+  @override
+  String toString() {
+    return 'UserModel(email: $email, uid: $uid, name: $name, profilePicture: $profilePicture, banner: $banner, username: $username, gender: $gender, accountType: $accountType, awards: $awards)';
+  }
+
+  @override
+  bool operator ==(covariant UserModel other) {
+    if (identical(this, other)) return true;
+  
+    return 
+      other.email == email &&
+      other.uid == uid &&
+      other.name == name &&
+      other.profilePicture == profilePicture &&
+      other.banner == banner &&
+      other.username == username &&
+      other.gender == gender &&
+      other.accountType == accountType &&
+      listEquals(other.awards, awards);
+  }
+
+  @override
+  int get hashCode {
+    return email.hashCode ^
+      uid.hashCode ^
+      name.hashCode ^
+      profilePicture.hashCode ^
+      banner.hashCode ^
+      username.hashCode ^
+      gender.hashCode ^
+      accountType.hashCode ^
+      awards.hashCode;
   }
 }
 
-//Add this in Register.dart -> add user to database
-// import this file as models;
-
-// models.User user = models.User(
-//     username : username,
-//     password : password,
-//     uid : cred.user!.uid,
-//     email : email,
-//     photoUrl : photoUrl,
-//     bio : bio,
-//     gender : gender,
-//     age : age,
-// );
-
-// await FirebaseFirestore.instance.collection('users').doc(cred.user!.uid).set(user.toJson(),);
