7f4be386ea3e34b20910fcf26ef8e9f825035c4e
ms2019 <ms2019@hw.ac.uk>
2023-01-30 14:32:43 +0400

    To be modified further

--- lib/providers/user_provider.dart
+++ lib/providers/user_provider.dart
@@ -3,17 +3,17 @@ import 'package:geotagar/models/users.dart';
 
 import '../services/auth.dart';
 
-class UserProvider with ChangeNotifier {
-  User? _user;
-  final AuthMethods _authMethods = AuthMethods();
+// class UserProvider with ChangeNotifier {
+//   UserModel? _user;
+//   final AuthMethods _authMethods = AuthMethods();
 
-  User get getUser => _user!;
-  Future<void> refreshUser() async {
-    User user = await _authMethods.getUserDetails();
-    _user = user;
-    notifyListeners();
-  }
-}
+//   UserModel get getUser => _user!;
+//   Future<void> refreshUser() async {
+//     UserModel user = await _authMethods.getUserDetails();
+//     _user = user;
+//     notifyListeners();
+//   }
+// }
 
 //Write in Register.dart
 // Future<model.USer> getUserDetails() async {
