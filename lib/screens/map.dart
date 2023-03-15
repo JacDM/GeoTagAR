c6f9127d311e6ef59d3928168951d1c22e55daa5
AamirNazir <aamirnazir299@gmail.com>
2023-03-15 14:07:25 +0400

    added geolocation dependancy for map

--- lib/screens/map.dart
+++ lib/screens/map.dart
@@ -1,7 +1,36 @@
 import 'dart:async';
+import 'package:geolocator/geolocator.dart';
 import 'package:flutter/material.dart';
 //import 'package:geolocator/geolocator.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
+
+Future<Position> _determinePosition() async {
+  bool serviceEnabled;
+  LocationPermission permission;
+
+  // Test if location services are enabled.
+  serviceEnabled = await Geolocator.isLocationServiceEnabled();
+  if (!serviceEnabled) {
+    // Location services are not enabled don't continue
+    // accessing the position and request users of the
+    // App to enable the location services.
+    return Future.error('Location services are disabled.');
+  }
+
+  permission = await Geolocator.checkPermission();
+  if (permission == LocationPermission.denied) {
+    permission = await Geolocator.requestPermission();
+    if (permission == LocationPermission.denied) {
+      return Future.error('Location permissions are denied');
+    }
+  }
+
+  if (permission == LocationPermission.deniedForever) {
+    return Future.error(
+        'Location permissions are permanently denied, we cannot request permissions.');
+  }
+  return await Geolocator.getCurrentPosition();
+}
 
 class Map extends StatefulWidget {
   const Map({Key? key}) : super(key: key);
@@ -13,7 +42,7 @@ class Map extends StatefulWidget {
 class _MapState extends State<Map> {
   Completer<GoogleMapController> _controller = Completer();
 
-  static const LatLng _center = const LatLng(25.2633, 	55.3087);
+  static const LatLng _center = const LatLng(25.2633, 55.3087);
 
   void _onMapCreated(GoogleMapController controller) {
     _controller.complete(controller);
