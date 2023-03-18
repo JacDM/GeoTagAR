8d9be10125a5421170c3e4336ba391dbb6dcf851
ms2019 <ms2019@hw.ac.uk>
2023-03-18 06:36:12 +0400

    Updated group model with moderator field

--- lib/models/groups.dart
+++ lib/models/groups.dart
@@ -7,14 +7,14 @@ class Community {
   final String groupBanner;
   final String groupPicture;
   final List<String> members;
-  //final List<String> mods;
+  final List<String> mods;
   Community({
     required this.groupId,
     required this.groupName,
     required this.groupBanner,
     required this.groupPicture,
     required this.members,
-    //required this.mods,
+    required this.mods,
   });
 
   static Community fromSnap(DocumentSnapshot snap) {
@@ -25,7 +25,8 @@ class Community {
         groupName: snapshot["groupName"],
         groupBanner: snapshot["groupBanner"],
         groupPicture: snapshot["groupPicture"],
-        members: snapshot["members"]);
+        members: snapshot["members"],
+        mods: snapshot["mods"]);
   }
 
   Map<String, dynamic> toJson() => {
@@ -36,23 +37,23 @@ class Community {
         "members": members,
       };
 
-  Community copyWith({
-    String? groupId,
-    String? groupName,
-    String? groupBanner,
-    String? groupPicture,
-    List<String>? members,
-    //List<String>? mods,
-  }) {
-    return Community(
-      groupId: groupId ?? this.groupId,
-      groupName: groupName ?? this.groupName,
-      groupBanner: groupBanner ?? this.groupBanner,
-      groupPicture: groupPicture ?? this.groupPicture,
-      members: members ?? this.members,
-      //mods: mods ?? this.mods,
-    );
-  }
+  // Community copyWith({
+  //   String? groupId,
+  //   String? groupName,
+  //   String? groupBanner,
+  //   String? groupPicture,
+  //   List<String>? members,
+  //   List<String>? mods,
+  // }) {
+  //   return Community(
+  //     groupId: groupId ?? this.groupId,
+  //     groupName: groupName ?? this.groupName,
+  //     groupBanner: groupBanner ?? this.groupBanner,
+  //     groupPicture: groupPicture ?? this.groupPicture,
+  //     members: members ?? this.members,
+  //     mods: mods ?? this.mods,
+  //   );
+  // }
 
   Map<String, dynamic> toMap() {
     return {
@@ -61,47 +62,9 @@ class Community {
       'groupBanner': groupBanner,
       'groupPicture': groupPicture,
       'members': members,
-      //'mods': mods,
+      'mods': mods,
     };
   }
 
-  factory Community.fromMap(Map<String, dynamic> map) {
-    return Community(
-      groupId: map['groupId'] ?? '',
-      groupName: map['groupName'] ?? '',
-      groupBanner: map['groupBanner'] ?? '',
-      groupPicture: map['groupPicture'] ?? '',
-      members: List<String>.from(map['members']),
-      //mods: List<String>.from(map['mods']),
-    );
-  }
-
-  @override
-  String toString() {
-    return 'Community(groupId: $groupId, groupName: $groupName, groupBanner: $groupBanner, groupPicture: $groupPicture, members: $members)';
-    // mods: $mods
-  }
-
-  @override
-  bool operator ==(Object other) {
-    if (identical(this, other)) return true;
-
-    return other is Community &&
-        other.groupId == groupId &&
-        other.groupName == groupName &&
-        other.groupBanner == groupBanner &&
-        other.groupPicture == groupPicture &&
-        listEquals(other.members, members);
-    //listEquals(other.mods, mods);
-  }
-
-  @override
-  int get hashCode {
-    return groupId.hashCode ^
-        groupName.hashCode ^
-        groupBanner.hashCode ^
-        groupPicture.hashCode ^
-        members.hashCode;
-    //mods.hashCode;
-  }
+//   }
 }
