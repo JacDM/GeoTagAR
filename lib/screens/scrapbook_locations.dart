1e5a772ea7d2b46a810e5d1277b0cf9bec86fb2e
AamirNazir <aamirnazir299@gmail.com>
2023-03-21 12:58:06 +0400

    aamir changes in map

--- lib/screens/scrapbook_locations.dart
+++ lib/screens/scrapbook_locations.dart
@@ -1,7 +1,9 @@
 // ignore_for_file: prefer_const_constructors
 
 import 'dart:async';
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
+import 'package:geotagar/services/firestore.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
 import '../../core/constants/constants.dart';
 
@@ -16,6 +18,7 @@ class scrapBookLocations extends StatefulWidget {
 // ignore: camel_case_types
 class _scrapBookLocationsState extends State<scrapBookLocations> {
   final Completer<GoogleMapController> _controller = Completer();
+  Map<MarkerId, Marker> markers = <MarkerId, Marker>{};
   static const LatLng sBook1 = LatLng(37.33500926, -122.03272188);
   static const LatLng sBook2 = LatLng(37.33429383, -122.06600055);
   BitmapDescriptor sBookIcon = BitmapDescriptor.defaultMarker;
@@ -33,6 +36,29 @@ class _scrapBookLocationsState extends State<scrapBookLocations> {
   void initState() {
     setCustomMarkerIcon();
     super.initState();
+  }
+
+  void initMarker(specify, specifyId) async {
+    var markerIdVal = specifyId;
+    final MarkerId markerId = MarkerId(markerIdVal);
+    final Marker marker = Marker(
+      markerId: markerId,
+      position:
+          LatLng(specify['location'].latitude, specify['location'].longitude),
+      //icon:
+    );
+    setState(() {
+      markers[markerId] = marker;
+    });
+  }
+
+  getMarkerData() async {
+    var postData = FirebaseFirestore.instance.collection('posts').get();
+    if (postData.toString().isNotEmpty) {
+      for (int i = 0; i < postData.toString().length; i++) {
+        initMarker(postData.document(), i);
+      }
+    }
   }
 
   @override
@@ -60,28 +86,25 @@ class _scrapBookLocationsState extends State<scrapBookLocations> {
       appBar: width > 600
           ? null
           : AppBar(
-          backgroundColor: Color.fromARGB(255, 29, 29, 29),
-          automaticallyImplyLeading: false,
-          centerTitle: true,
-          toolbarHeight: 90,
-          title: Row(
-            children: [
-              Image.asset(
-                Constants.logoPathBlack,
-                height: 200.0,
-                width: 200.0,
-
-              ),
-
-              const Text(
-                'NEAR YOU',
-                style: TextStyle(fontSize: 34, ),
-              ),
-
-            ],
-          )
-
-      ),
+              backgroundColor: Color.fromARGB(255, 29, 29, 29),
+              automaticallyImplyLeading: false,
+              centerTitle: true,
+              toolbarHeight: 90,
+              title: Row(
+                children: [
+                  Image.asset(
+                    Constants.logoPathBlack,
+                    height: 200.0,
+                    width: 200.0,
+                  ),
+                  const Text(
+                    'NEAR YOU',
+                    style: TextStyle(
+                      fontSize: 34,
+                    ),
+                  ),
+                ],
+              )),
       body: GoogleMap(
         initialCameraPosition: const CameraPosition(
           target: sBook1,
