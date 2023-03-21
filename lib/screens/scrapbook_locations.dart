8dfa0d44b47114f08e961b9147e2f6d85ea890c0
AamirNazir <aamirnazir299@gmail.com>
2023-03-21 20:33:39 +0400

    aamir map done

--- lib/screens/scrapbook_locations.dart
+++ lib/screens/scrapbook_locations.dart
@@ -1,4 +1,4 @@
-// ignore_for_file: prefer_const_constructors
+// ignore_for_file: prefer_const_constructors, prefer_collection_literals
 
 import 'dart:async';
 import 'package:cloud_firestore/cloud_firestore.dart';
@@ -6,6 +6,8 @@ import 'package:flutter/material.dart';
 import 'package:geotagar/services/firestore.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
 import '../../core/constants/constants.dart';
+
+const double ZOOM = 1;
 
 // ignore: camel_case_types
 class scrapBookLocations extends StatefulWidget {
@@ -18,7 +20,6 @@ class scrapBookLocations extends StatefulWidget {
 // ignore: camel_case_types
 class _scrapBookLocationsState extends State<scrapBookLocations> {
   final Completer<GoogleMapController> _controller = Completer();
-  Map<MarkerId, Marker> markers = <MarkerId, Marker>{};
   static const LatLng sBook1 = LatLng(37.33500926, -122.03272188);
   static const LatLng sBook2 = LatLng(37.33429383, -122.06600055);
   BitmapDescriptor sBookIcon = BitmapDescriptor.defaultMarker;
@@ -38,94 +39,90 @@ class _scrapBookLocationsState extends State<scrapBookLocations> {
     super.initState();
   }
 
-  void initMarker(specify, specifyId) async {
-    var markerIdVal = specifyId;
-    final MarkerId markerId = MarkerId(markerIdVal);
-    final Marker marker = Marker(
-      markerId: markerId,
-      position:
-          LatLng(specify['location'].latitude, specify['location'].longitude),
-      //icon:
-    );
-    setState(() {
-      markers[markerId] = marker;
-    });
-  }
-
-  getMarkerData() async {
-    var postData = FirebaseFirestore.instance.collection('posts').get();
-    if (postData.toString().isNotEmpty) {
-      for (int i = 0; i < postData.toString().length; i++) {
-        initMarker(postData, i);
-      }
-    }
-  }
-
+  //static GoogleMapController _googleMapController;
+  Set<Marker> markers = Set();
   @override
   Widget build(BuildContext context) {
     final width = MediaQuery.of(context).size.width;
     return Scaffold(
-      // appBar: AppBar(
-      //   centerTitle: false,
-      //   toolbarHeight: 90,
-      //   elevation: 3,
-      //   //used to remove the back button from appBar
-      //   automaticallyImplyLeading: false,
-      //   backgroundColor: Colors.black,
-      //   title: const Text(
-      //     "Near You",
-      //     style: TextStyle(
-      //       color: Colors.white,
-      //       fontSize: 30,
-      //       fontWeight: FontWeight.w900,
-      //       //fontFamily: 'arial',
-      //     ),
-      //   ),
-      // ),
+        // appBar: AppBar(
+        //   centerTitle: false,
+        //   toolbarHeight: 90,
+        //   elevation: 3,
+        //   //used to remove the back button from appBar
+        //   automaticallyImplyLeading: false,
+        //   backgroundColor: Colors.black,
+        //   title: const Text(
+        //     "Near You",
+        //     style: TextStyle(
+        //       color: Colors.white,
+        //       fontSize: 30,
+        //       fontWeight: FontWeight.w900,
+        //       //fontFamily: 'arial',
+        //     ),
+        //   ),
+        // ),
 
-      appBar: width > 600
-          ? null
-          : AppBar(
-              backgroundColor: Color.fromARGB(255, 29, 29, 29),
-              automaticallyImplyLeading: false,
-              centerTitle: true,
-              toolbarHeight: 90,
-              title: Row(
-                children: [
-                  Image.asset(
-                    Constants.logoPathBlack,
-                    height: 200.0,
-                    width: 200.0,
-                  ),
-                  const Text(
-                    'NEAR YOU',
-                    style: TextStyle(
-                      fontSize: 34,
+        appBar: width > 600
+            ? null
+            : AppBar(
+                backgroundColor: Color.fromARGB(255, 29, 29, 29),
+                automaticallyImplyLeading: false,
+                centerTitle: true,
+                toolbarHeight: 90,
+                title: Row(
+                  children: [
+                    Image.asset(
+                      Constants.logoPathBlack,
+                      height: 200.0,
+                      width: 200.0,
                     ),
-                  ),
-                ],
-              )),
-      body: GoogleMap(
-        initialCameraPosition: const CameraPosition(
-          target: sBook1,
-          zoom: 13.5,
-        ),
-        markers: {
-          Marker(
-            markerId: MarkerId("Dave's Scrapbook"),
-            position: sBook1,
-            icon: sBookIcon,
-          ),
-          Marker(
-            markerId: MarkerId("Heriot Watt ScrapBook"),
-            position: sBook2,
-            icon: sBookIcon,
-          ),
-        },
-        onMapCreated: (mapController) {
-          _controller.complete(mapController);
-        },
-      ),
-    );
+                    const Text(
+                      'NEAR YOU',
+                      style: TextStyle(
+                        fontSize: 34,
+                      ),
+                    ),
+                  ],
+                )),
+        body: StreamBuilder(
+            stream:
+                FirebaseFirestore.instance.collection("Location").snapshots(),
+            builder: (context, snapshot) {
+              print(snapshot);
+              if (snapshot.hasData) {
+                //Extract the location from document
+                GeoPoint location = snapshot.data!.docs.first.get("location");
+
+                // Check if location is valid
+                if (location == null) {
+                  return Text("There was no location data");
+                }
+
+                // Remove any existing markers
+                markers.clear();
+
+                final latLng = LatLng(location.latitude, location.longitude);
+
+                // Add new marker with markerId.
+                markers.add(
+                    Marker(markerId: MarkerId("location"), position: latLng));
+
+                return GoogleMap(
+                  initialCameraPosition: CameraPosition(
+                      target: LatLng(location.latitude, location.longitude)),
+                  // Markers to be pointed
+                  markers: markers,
+                  // onMapCreated: (controller) {
+                  //   // Assign the controller value to use it later
+                  //   controller = controller;
+                  // },
+                );
+              } else {
+                return Center(
+                  child: CircularProgressIndicator(),
+                );
+              }
+            }));
   }
 }
