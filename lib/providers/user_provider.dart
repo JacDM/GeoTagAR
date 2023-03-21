6ad0a1b62e174f1df8c3e349589725f39187cd02
ms2019 <ms2019@hw.ac.uk>
2023-03-21 23:33:57 +0400

    Commented out provider

--- lib/providers/user_provider.dart
+++ lib/providers/user_provider.dart
@@ -1,27 +1,27 @@
-import 'package:firebase_auth/firebase_auth.dart';
-import 'package:flutter/widgets.dart';
-import 'package:flutter/material.dart';
-import 'package:geotagar/models/users.dart';
-import '../services/auth.dart';
+// import 'package:firebase_auth/firebase_auth.dart';
+// import 'package:flutter/widgets.dart';
+// import 'package:flutter/material.dart';
+// import 'package:geotagar/models/users.dart';
+// import '../services/auth.dart';
 
-class UserProvider with ChangeNotifier {
-  UserModel? _user;
-  final AuthServices _authServices = AuthServices();
+// class UserProvider with ChangeNotifier {
+//   UserModel? _user;
+//   final AuthServices _authServices = AuthServices();
 
-  UserModel get getUser => _user!;
+//   UserModel get getUser => _user!;
 
-  Future<void> refreshUser() async {
-    UserModel user = await _authServices.getUserDetails();
-    _user = user;
-    notifyListeners();
-  }
-}
+//   Future<void> refreshUser() async {
+//     UserModel user = await _authServices.getUserDetails();
+//     _user = user;
+//     notifyListeners();
+//   }
+// }
 
-//Write in Register.dart
-// Future<model.USer> getUserDetails() async {
-//     User currentUser = FirebaseAuth.instance.currentUser!;
-//     DocumentSnapshot snap = await FirebaseFirestore.instance.collection('users').doc(currentUser.uid).get();
-//     return models.User.fromSnap(snap);
+// //Write in Register.dart
+// // Future<model.USer> getUserDetails() async {
+// //     User currentUser = FirebaseAuth.instance.currentUser!;
+// //     DocumentSnapshot snap = await FirebaseFirestore.instance.collection('users').doc(currentUser.uid).get();
+// //     return models.User.fromSnap(snap);
 
     
-// }
+// // }
