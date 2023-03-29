492bc106174fbcbcffd012a326c6fb1988b75855
ms2019 <ms2019@hw.ac.uk>
2023-03-29 08:04:35 +0400

    Removed unused imports

--- lib/utils/global_variables.dart
+++ lib/utils/global_variables.dart
@@ -1,15 +1,10 @@
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:geotagar/screens/ARLauncher.dart';
-import 'package:geotagar/screens/admin_pages/admin_profile.dart';
 import 'package:geotagar/screens/admin_pages/groups_page.dart';
 import 'package:geotagar/screens/discoverPages/discover.dart';
 import 'package:geotagar/screens/feed.dart';
-import 'package:geotagar/screens/memory_related/createMemoryRoute.dart';
 import 'package:geotagar/screens/scrapbook_locations.dart';
-import 'package:geotagar/screens/memory_related/post_memory.dart';
-import 'package:image_picker/image_picker.dart';
-import 'package:geotagar/ar/ar.dart';
 import '../screens/admin_pages/report_page.dart';
 import '../screens/userAccountScreens/user_profile.dart';
 // import 'package:instagram_clone_flutter/screens/add_post_screen.dart';
