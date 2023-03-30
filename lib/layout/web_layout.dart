ad737045ba06a3ff92cf112945e8e22258e339ad
ms2019 <ms2019@hw.ac.uk>
2023-03-30 11:02:46 +0400

    Removed extra tab

--- lib/layout/web_layout.dart
+++ lib/layout/web_layout.dart
@@ -1,4 +1,3 @@
-import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/flutter_svg.dart';
 import 'package:geotagar/utils/colors.dart';
@@ -84,120 +83,90 @@ class _WebScreenLayoutState extends State<WebScreenLayout> {
         ],
       );
     } else {
-    
-    
-    return Container(
-      decoration: BoxDecoration(
-        image: DecorationImage(
-          image: AssetImage("assets/images/forgotPasswordBG.png"),
-          fit: BoxFit.cover,
+      return Container(
+        decoration: const BoxDecoration(
+          image: DecorationImage(
+            image: AssetImage("assets/images/forgotPasswordBG.png"),
+            fit: BoxFit.cover,
+          ),
         ),
-      ),
-      child: (userData['isAdmin']) //userData['isAdmin']=='true')
-          ? Scaffold(
-              //admin
-              appBar: AppBar(
-                backgroundColor: Color.fromARGB(31, 0, 0, 0),
-                automaticallyImplyLeading: false,
-                centerTitle: false,
-                title: const Text("Reported Posts"),
-                actions: [
-                  IconButton(
-                    icon: Icon(
-                      Icons.report_outlined,
-                      color: _page == 0 ? Colors.green : Pallete.whiteColor,
-                    ),
-                    onPressed: () => navigationTapped(0),
-                  ),
-                  IconButton(
-                    icon: Icon(
-                      Icons.group_remove_outlined,
-                      color: _page == 1 ? Colors.green : Pallete.whiteColor,
-                    ),
-                    onPressed: () => navigationTapped(1),
-                  ),
-                  // IconButton(
-                  //   icon: Icon(
-                  //     Icons.add_a_photo,
-                  //     color: _page == 2 ? Colors.green : Pallete.whiteColor,
-                  //   ),
-                  //   onPressed: () => navigationTapped(2),
-                  // ),
-                  // IconButton(
-                  //   icon: Icon(
-                  //     Icons.groups,
-                  //     color: _page == 2 ? Colors.green : Pallete.whiteColor,
-                  //   ),
-                  //   onPressed: () => navigationTapped(2),
-                  // ),
-                  // IconButton(
-                  //   icon: Icon(
-                  //     Icons.admin_panel_settings,
-                  //     color: _page == 3 ? Colors.green : Pallete.whiteColor,
-                  //   ),
-                  //   onPressed: () => navigationTapped(3),
-                  // ),
-                ],
-              ),
-              body: PageView(
-                physics: const NeverScrollableScrollPhysics(),
-                children: webScreenItems,
-                controller: pageController,
-                onPageChanged: onPageChanged,
-              ),
-            )
-          : Scaffold( 
-              appBar: AppBar(
-                automaticallyImplyLeading: false,
-                backgroundColor:
-                    Pallete.darkModeAppTheme.scaffoldBackgroundColor,
-                centerTitle: false,
-                title: SvgPicture.asset(
-                  'assets/images/forgotPasswordBG.png',
-                  color: Pallete.greyColor,
-                  height: 32,
-                ),
-                actions: [
-                  IconButton(
-                      onPressed: () => navigationTapped(0),
+        child: (userData['isAdmin']) //userData['isAdmin']=='true')
+            ? Scaffold(
+                //admin
+                appBar: AppBar(
+                  backgroundColor: Color.fromARGB(31, 0, 0, 0),
+                  automaticallyImplyLeading: false,
+                  centerTitle: false,
+                  title: const Text("Reported Posts"),
+                  actions: [
+                    IconButton(
                       icon: Icon(
-                        Icons.home,
+                        Icons.report_outlined,
                         color: _page == 0 ? Colors.green : Pallete.whiteColor,
-                      )),
-                  IconButton(
-                      onPressed: () => navigationTapped(1),
-                      icon: Icon(
-                        Icons.search,
-                        color: _page == 1 ? Colors.green : Pallete.whiteColor,
-                      )),
-                  IconButton(
-                      onPressed: () => navigationTapped(2),
-                      icon: Icon(
-                        Icons.add_a_photo,
-                        color: _page == 2 ? Colors.green : Pallete.whiteColor,
-                      )),
-                  IconButton(
-                      onPressed: () => navigationTapped(3),
-                      icon: Icon(
-                        Icons.favorite,
-                        color: _page == 3 ? Colors.green : Pallete.whiteColor,
-                      )),
-                  IconButton(
-                      onPressed: () => navigationTapped(4),
-                      icon: Icon(
-                        Icons.person,
-                        color: _page == 4 ? Colors.green : Pallete.whiteColor,
-                      )),
-                ],
-              ),
-              body: PageView(
-                physics: const NeverScrollableScrollPhysics(),
-                children: homeScreenItems,
-                controller: pageController,
-                onPageChanged: onPageChanged,
-              ),
-            ), //normal user
-    );
+                      ),
+                      onPressed: () => navigationTapped(0),
+                    ),
+                  ],
+                ),
+                body: PageView(
+                  physics: const NeverScrollableScrollPhysics(),
+                  children: webScreenItems,
+                  controller: pageController,
+                  onPageChanged: onPageChanged,
+                ),
+              )
+            : Scaffold(
+                appBar: AppBar(
+                  automaticallyImplyLeading: false,
+                  backgroundColor:
+                      Pallete.darkModeAppTheme.scaffoldBackgroundColor,
+                  centerTitle: false,
+                  title: SvgPicture.asset(
+                    'assets/images/forgotPasswordBG.png',
+                    color: Pallete.greyColor,
+                    height: 32,
+                  ),
+                  actions: [
+                    IconButton(
+                        onPressed: () => navigationTapped(0),
+                        icon: Icon(
+                          Icons.home,
+                          color: _page == 0 ? Colors.green : Pallete.whiteColor,
+                        )),
+                    IconButton(
+                        onPressed: () => navigationTapped(1),
+                        icon: Icon(
+                          Icons.search,
+                          color: _page == 1 ? Colors.green : Pallete.whiteColor,
+                        )),
+                    IconButton(
+                        onPressed: () => navigationTapped(2),
+                        icon: Icon(
+                          Icons.add_a_photo,
+                          color: _page == 2 ? Colors.green : Pallete.whiteColor,
+                        )),
+                    IconButton(
+                        onPressed: () => navigationTapped(3),
+                        icon: Icon(
+                          Icons.favorite,
+                          color: _page == 3 ? Colors.green : Pallete.whiteColor,
+                        )),
+                    IconButton(
+                        onPressed: () => navigationTapped(4),
+                        icon: Icon(
+                          Icons.person,
+                          color: _page == 4 ? Colors.green : Pallete.whiteColor,
+                        )),
+                  ],
+                ),
+                body: PageView(
+                  physics: const NeverScrollableScrollPhysics(),
+                  children: homeScreenItems,
+                  controller: pageController,
+                  onPageChanged: onPageChanged,
+                ),
+              ), //normal user
+      );
+    }
   }
-}
 }
