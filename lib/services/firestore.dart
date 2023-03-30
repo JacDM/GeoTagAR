5a169b1f08069da600718da4232b5533c6ab5fae
ms2019 <ms2019@hw.ac.uk>
2023-03-30 08:33:55 +0400

    Added value of locked attribute to posts

--- lib/services/firestore.dart
+++ lib/services/firestore.dart
@@ -34,7 +34,7 @@ class FireStoreMethods {
         datePublished: DateTime.now(),
         postUrl: photoUrl,
         profImage: profImage,
-        location: GeoPoint(lat, long),
+        location: GeoPoint(lat, long), locked: false,
       );
       _firestore.collection('posts').doc(postId).set(post.toJson());
       res = "success";
